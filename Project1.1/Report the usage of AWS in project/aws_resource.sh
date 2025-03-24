cf955d235c05dc51b18ffe3acc0e6a15dac7e562
iam-amolgosavi <149919930+iam-amolgosavi@users.noreply.github.com>
2025-03-24 14:48:43 +0530

    Update aws_resource.sh

--- Project1.1/Report the usage of AWS in project/aws_resource.sh
+++ Project1.1/Report the usage of AWS in project/aws_resource.sh
@@ -6,24 +6,21 @@
 
 #Aws s3, Aws EC2,Aws Lambda, Aws IAM USer
 
-set -x
+set -x  # it use for print the command also OR dubugging
 
 #list s3 bucket
 echo "print the s3 buckets"
-
 aws s3 ls
 
 #Ec2 Instances list
 echo " printh ec2 instances"
-
 aws ec2 describe-instances
 
 #aws list lambda
 echo "print rhe lambda functions"
-
 aws lambda list-functions
+
 #List IAM user
 echo "print Iam list of user"
-
 aws iam list-users
 
