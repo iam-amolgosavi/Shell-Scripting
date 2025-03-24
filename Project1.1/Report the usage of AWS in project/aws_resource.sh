fb7e58addb146b3892d145db6593a7fc4099553d
iam-amolgosavi <149919930+iam-amolgosavi@users.noreply.github.com>
2025-03-24 14:45:08 +0530

    Create aws_resource.sh

---
+++ Project1.1/Report the usage of AWS in project/aws_resource.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+#Author: Amol
+#Date: 24-march
+#This script will report aws resources usage
+
+#Aws s3, Aws EC2,Aws Lambda, Aws IAM USer
+
+set -x
+
+#list s3 bucket
+echo "print the s3 buckets"
+
+aws s3 ls
+
+#Ec2 Instances list
+echo " printh ec2 instances"
+
+aws ec2 describe-instances
+
+#aws list lambda
+echo "print rhe lambda functions"
+
+aws lambda list-functions
+#List IAM user
+echo "print Iam list of user"
+
+aws iam list-users
+
